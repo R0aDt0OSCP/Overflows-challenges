287c66bbe2101a5d024bcef864a19fbd76b49666
R0aDt0OSCP <112269329+R0aDt0OSCP@users.noreply.github.com>
2022-09-25 16:02:01 +0530

    Create checkint.c

---
+++ checkint.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+ int menu;
+ printf("\n Enter a number of your  selection\n");
+  scanf("%d", &menu);
+    printf("\nThe number choosen %d\n", menu);
+    }
+ 
